c31e5d72bd51419cb8c88a8f16f6980438468193
Rushil Lakhani Lakhani <alu4093@cc443.(none)>
2013-11-20 14:05:52 +0000

    Añadiendo operaciones

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -3,7 +3,7 @@
 class Matriz
   require "./lib/racional.rb"
 
-  attr_reader :nfil, :ncol # metodos de acceso (getter)
+  attr_reader :nfil, :ncol, :mat # metodos de acceso (getter)
   
   # metodo que inicializa la matriz 
   
@@ -31,11 +31,6 @@ class MatrizDensa < Matriz
     return mat[i]
   end
 
-#Funcion que devuelve una posicion i,j dentro de la matriz  
-  
-  def [](i,j)
-      return mat[i][j]
-   end
    
 #Funcion que asigna un valor k a una posicion i,j dentro de la matriz
    
@@ -102,18 +97,54 @@ end
 
 
 class MatrizDispersa < Matriz
-end
+  attr_reader:hash , :mat
+  def initialize (nfil, ncol, mat)
+    super(nfil, ncol)
+    @mat = Array.new(mat)
+    nceros = 0 # numero de elementos nulos de la matriz (0)
+    nelementos= (nfil * ncol)*0.6 # elementos de la matriz aplicado el 60 % 
+    psincero = 0 # posiciones de los elementos de la matriz cuyo valor no es nulo (0)
+    @hash = Hash.new(0)
+    for i in 0..nfil do
+       for j in 0..ncol do
+           if (mat[i][j]==0)  
+               nceros=nceros+1
+            else
+               # hash
+	       print "Valores"
+               puts psincero="[#{i}][#{j}]"
+               @hash[psincero]=mat[i][j]
+	       puts hash.length
+	       puts hash
+            end
+       end
+    end
+      if nceros >= nelementos
+	 puts "La matriz es dispersa" 
+      else
+	 raise ArgumentError, 'La Matriz no es dispersa'
+      end
+ end
+end 
+
 
 
 
 
    
 #end 
+
 frac1 = Fraccion.new(1,3)
+#puts frac1+1
+puts 1-frac1 
+puts frac1-1
 frac2 = Fraccion.new(1,4)
 m1 = MatrizDensa.new(2,2,[[1,2],[3,4]])
+puts m1[0].size
+puts m1[1].size
 m2 = MatrizDensa.new(2,2,[[7,10],[15,22]])
 m3 = MatrizDensa.new(2,2,[[frac1,frac2],[frac1,frac2]])
+m4 = MatrizDispersa.new(2, 2, [[0,0,1],[1,2,3],[0,0,1]])
 puts "     M1   "
 puts m1.to_s
 puts "     M2   "
